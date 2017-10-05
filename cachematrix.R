9a6ece195651f1745d7c3f45221d04282d9290f9
npreyn <32079243+npreyn@users.noreply.github.com>
2017-10-05 11:21:12 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function allows you to input a square matrix and calculate it's inverse
+## while cache the result so that the same matrix does not have to be calculated
+## a second time.
 
-## Write a short comment describing this function
+## This function creates 2 objects (x, sol) and 4 functions (set, get, setsol, getsol)
+## that are then made avaialble to the global environment.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        sol <- NULL
+        set <- function(y) {
+                x <<- y
+                sol <<- NULL
+        }
+        get <- function()x
+        setsol <- function(solve) sol <<- solve
+        getsol <- function() sol
+        list(set = set, get = get,
+             setsol = setsol,
+             getsol = getsol)
 }
 
 
-## Write a short comment describing this function
+## This function uses the objects created by makeCacheMatrix and, using the new
+## function, solve, calculates the inverse of the matrix that was input as an argument
+## in makeCacheMatrix, but only if the inverse hasn't already been calculated in the 
+## desiganted environment.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        sol <- x$getsol()
+        if(!is.null(sol)) {
+                message("getting cached data")
+                return(sol)
+        }
+        data <- x$get()
+        sol <- solve(data,...)
+        x$setsol(sol)
+        sol
 }
